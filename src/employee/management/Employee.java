32fc3174eb8b06d5e200401c6527aea51f56a92e
jayalloyd <jayaranilloyd1983@gmail.com>
2024-03-25 22:04:08 +0530

    first commit

---
+++ src/employee/management/Employee.java
@@ -0,0 +1,35 @@
+package employee.management;
+
+public class Employee {
+
+	private String emp_name;
+	private int emp_id;
+    private String emp_dept;
+    private String emp_position;
+	public String getEmp_name() {
+		return emp_name;
+	}
+	public void setEmp_name(String emp_name) {
+		this.emp_name = emp_name;
+	}
+	public int getEmp_id() {
+		return emp_id;
+	}
+	public void setEmp_id(int emp_id) {
+		this.emp_id = emp_id;
+	}
+	public String getEmp_dept() {
+		return emp_dept;
+	}
+	public void setEmp_dept(String emp_dept) {
+		this.emp_dept = emp_dept;
+	}
+	public String getEmp_position() {
+		return emp_position;
+	}
+	public void setEmp_position(String emp_position) {
+		this.emp_position = emp_position;
+	}
+    
+    
+}
